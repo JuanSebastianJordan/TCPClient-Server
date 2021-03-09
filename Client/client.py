a9bb3b7935a06600835acde5217e3b7a0e7b12c4
Andres Bayona <al.bayona@uniandes.edu.co>
2021-03-09 13:59:42 -0500

    Client

--- Client/client.py
+++ Client/client.py
@@ -1,23 +1,100 @@
 # Code for the client
 ##
+import hashlib
 import socket
 
 ClientSocket = socket.socket()
 host = '127.0.0.1'
 port = 1233
+BufferSize = 1024
+
+File_path = "./data/media/"
+
+SYN = 'Hello'
+AKN = 'Ready'
+AKN_NAME = 'Name'
+AKN_OK = 'Ok'
+AKN_HASH = 'HashOk'
 
 print('Waiting for connection')
+
+
+def hash_file(file):
+    """"This function returns the SHA-1 hash
+    of the file passed into it"""
+    # make a hash object
+    h = hashlib.sha1()
+
+    # open file for reading in binary mode
+
+    # loop till the end of the file
+    chunk = 0
+    while chunk != b'':
+        # read only 1024 bytes at a time
+        chunk = file.read(BufferSize)
+        h.update(chunk)
+
+    # return the hex representation of digest
+    return h.hexdigest()
+
+
+
 try:
     ClientSocket.connect((host, port))
 except socket.error as e:
     print(str(e))
 
-Response = ClientSocket.recv(1024)
+Response = ClientSocket.recv(BufferSize)
 while True:
 
-    ClientSocket.send(str.encode("Hello"))
-    Response = ClientSocket.recv(1024)
+    try:
+        ClientSocket.send(str.encode(SYN))
+        Response = ClientSocket.recv(BufferSize)
 
-    print(Response.decode('utf-8'))
+        file_name = ''
 
-ClientSocket.close()
+        if Response == SYN:
+            print("Client Says: Hail back from server")
+
+            ClientSocket.send(str.encode(AKN))
+
+            Response = ClientSocket.recv(BufferSize).decode('utf-8')
+
+            if Response:
+
+                print("Client Says: file name received from server")
+                file_name = Response
+                ClientSocket.send(str.encode(AKN_NAME))
+
+                Response = ClientSocket.recv(BufferSize).decode('utf-8')
+
+                if Response:
+
+                    print("Client Says: file hash received from server")
+                    serverHash = Response
+                    ClientSocket.send(str.encode(AKN_OK))
+
+
+                    with open(File_path + file_name, 'wb') as f:
+                        print("Client Says: file opened")
+                        while True:
+
+                            data = ClientSocket.recv(BufferSize)
+                            print("Client Says: file chuck received from server: {}".format(data))
+                            if not data:
+                                f.close()
+                                print("Client Says: file data stream is complete")
+                                break
+                            # write data to a file
+                            f.write(data)
+
+                    if
+
+    except Exception as err:
+        connection.close()
+        print("Client Says: Error during connection with server")
+        traceback.print_tb(err.__traceback__)
+
+    ClientSocket.close()
+
+
--- Server/server.py
+++ Server/server.py
@@ -3,6 +3,7 @@
 import hashlib
 import socket
 import os
+import traceback
 from _thread import *
 
 host = '127.0.0.1'
@@ -19,6 +20,7 @@ SYN = 'Hello'
 AKN = 'Ready'
 AKN_NAME = 'Name'
 AKN_OK = 'Ok'
+AKN_HASH = 'HashOk'
 
 ServerSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 try:
@@ -34,40 +36,40 @@ def threaded_client(connection, idThread):
     connection.send(str.encode('Welcome to the Server'))
     while True:
 
-
-        reply = connection.recv(BufferSize).decode('utf-8')
-        if reply == SYN:
-            print("Server Says: Hail from client {} received".format(idThread))
-
-            connection.sendall(str.encode(reply))
+        try:
             reply = connection.recv(BufferSize).decode('utf-8')
+            if reply == SYN:
+                print("Server Says: Hail from client {} received".format(idThread))
 
-            if reply == AKN:
-                print("Server Says: Sending file name ({}) to client {}".format(File_name, idThread))
-                connection.sendall(str.encode(File_name))
+                connection.send(str.encode(SYN))
                 reply = connection.recv(BufferSize).decode('utf-8')
 
-                if reply == AKN_NAME:
-                    hash = hash_file(File)
-                    print("Server Says: Sending file hash to client {}".format(hash))
-                    connection.sendall(str.encode(hash))
-
+                if reply == AKN:
+                    print("Server Says: Sending file name ({}) to client {}".format(File_name, idThread))
+                    connection.sendall(str.encode(File_name))
                     reply = connection.recv(BufferSize).decode('utf-8')
-                    if reply == AKN_OK:
 
-                        connection.sendall(str.encode(hash))
+                    if reply == AKN_NAME:
+                        hash = hash_file(File)
+                        print("Server Says: Sending file hash to client {}".format(hash))
+                        connection.send(str.encode(hash))
 
-        else:
+                        reply = connection.recv(BufferSize).decode('utf-8')
+                        if reply == AKN_OK:
+                            send_file(connection, idThread)
+
+                            reply = connection.recv(BufferSize).decode('utf-8')
+                            if reply == AKN_HASH:
+                                print("Server Says: File successfully sent to client {}".format(hash))
+                                connection.close()
+
             print("Server Says: Unable to connect to client {}".format(idThread))
             connection.close()
 
-        send_file(connection)
-
-        print(reply)
-
-        connection.sendall(str.encode(reply))
-
-    connection.close()
+        except Exception as err:
+            connection.close()
+            print("Server Says: Error during connection with client {}".format(idThread))
+            traceback.print_tb(err.__traceback__)
 
 
 def send_file(connection, idThread):
