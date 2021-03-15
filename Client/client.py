0afc7bbd8dbee172bebbdfb80fbbb2aea89aadc9
Andres Bayona <al.bayona@uniandes.edu.co>
2021-03-15 07:29:49 -0500

    Changes

--- Client/client.py
+++ Client/client.py
@@ -4,8 +4,10 @@ import hashlib
 import math
 import socket
 import traceback
+from concurrent.futures import ThreadPoolExecutor
+import time
 
-ClientSocket = socket.socket()
+
 host = '127.0.0.1'
 port = 1233
 BufferSize = 1024
@@ -20,68 +22,85 @@ AKN_HASH = 'HashOk'
 ERROR = 'Error'
 AKN_COMPLETE = 'SendComplete'
 
-print('Waiting for connection')
 
+class ClientProtocol:
 
+    def __init__(self):
+        print('Waiting for connection')
 
+        self.client_socket = socket.socket()
 
+        try:
+            self.client_socket.connect((host, port))
+        except socket.error as e:
+            print('Client Says: error creating socket ', str(e))
 
-def hash_file():
-    with open(File_path + file_name, 'rb') as file:
-        """"This function returns the SHA-1 hash
-        of the file passed into it"""
-        # make a hash object
-        h = hashlib.sha1()
+    def hash_file(self, file_name):
+        with open(File_path + file_name, 'rb') as file:
+            """"This function returns the SHA-1 hash
+            of the file passed into it"""
+            # make a hash object
+            h = hashlib.sha1()
 
-        # open file for reading in binary mode
+            # open file for reading in binary mode
 
-        # loop till the end of the file
-        chunk = 0
-        while chunk != b'':
-            # read only 1024 bytes at a time
-            chunk = file.read(BufferSize)
-            h.update(chunk)
-        file.close()
+            # loop till the end of the file
+            chunk = 0
+            while chunk != b'':
+                # read only 1024 bytes at a time
+                chunk = file.read(BufferSize)
+                h.update(chunk)
+            file.close()
 
-    # return the hex representation of digest
-    return h.hexdigest()
+        # return the hex representation of digest
+        return h.hexdigest()
 
+    def receive_from_server(self):
+        return self.client_socket.recv(BufferSize).decode('utf-8')
 
+    def send_to_server(self, segment, print_message):
+        self.client_socket.send(str.encode(segment))
+        print(print_message)
 
-try:
-    ClientSocket.connect((host, port))
-except socket.error as e:
-    print(str(e))
+    def verify_reply(self, received, expected):
+        if not expected == received:
+            raise Exception("Error in protocol: expected {}; received {}".format(expected, received))
 
-while True:
+    def verify_reply_not_null(self, reply, expected):
+        if not reply:
+            raise Exception("Error in protocol: expected {}; received nothing".format(expected))
 
-    try:
-        ClientSocket.send(str.encode(SYN))
-        Response = ClientSocket.recv(BufferSize).decode('utf-8')
+    def run(self):
 
-        if Response == SYN:
-            print("Client Says: Hail back from server")
+        while True:
 
-            ClientSocket.send(str.encode(AKN))
-        else:
-            print("Client Says: Error in protocol with server: {}".format(Response))
-            ClientSocket.send(str.encode(ERROR))
+            try:
+                self.send_to_server(SYN, "Client Says: Hail sent to server")
 
-        Response = ClientSocket.recv(BufferSize).decode('utf-8')
+                reply = self.receive_from_server()
 
-        if Response:
+                self.verify_reply(reply, SYN)
+                print("Client Says: Hail back from server")
 
-            file_name = Response
-            print("Client Says: file name received from server: {}".format(file_name))
-            ClientSocket.send(str.encode(AKN_NAME))
+                self.send_to_server(AKN, "Client Says: communicating to server that client is ready for file transport")
 
-            Response = ClientSocket.recv(BufferSize).decode('utf-8')
-            if Response:
-                serverHash = Response
-                print("Client Says: file hash received from server: {}".format(serverHash))
-                ClientSocket.send(str.encode(AKN_OK))
+                reply = self.receive_from_server()
 
-                size = int(ClientSocket.recv(BufferSize).decode('utf-8'))
+                self.verify_reply_not_null(reply, 'file name')
+
+                file_name = reply
+
+                self.send_to_server(AKN_NAME, "Client Says: file name received from server: {}".format(file_name))
+
+                reply = self.receive_from_server()
+
+                self.verify_reply_not_null(reply, 'file hash')
+
+                serverHash = reply
+
+                self.send_to_server(AKN_OK, "Client Says: file hash received from server: {}".format(serverHash))
+
+                size = int(self.receive_from_server())
                 print("Client Says: file size received from server: {}".format(size))
 
                 with open(File_path + file_name, 'wb') as f:
@@ -89,30 +108,37 @@ while True:
 
                     for _ in range(math.ceil(size / BufferSize)):
                         # read only 1024 bytes at a time
-                        data = ClientSocket.recv(BufferSize)
-                        # print("Client Says: file chuck received from server: {}".format(data))
+                        data = self.client_socket.recv(BufferSize)
+                        print("Client Says: file chuck received from server: {}".format(data))
                         f.write(data)
 
-
                     f.close()
-                    print("Client Says: file transmissions complete")
+                    print("Client Says: file transmission is complete")
 
+                calculated_hash = self.hash_file(file_name)
+                is_valid = calculated_hash == serverHash
 
-                isValid = hash_file() == serverHash
-                print(isValid)
+                if is_valid:
 
-                if isValid:
-                    ClientSocket.send(str.encode(AKN_HASH))
-                    ClientSocket.close()
+                    self.send_to_server(AKN_HASH, "File integrity is verified with calculated hash {}".format(calculated_hash))
+                    self.client_socket.close()
                     break
                 else:
-                    ClientSocket.send(str.encode(ERROR))
-    except Exception as err:
-        ClientSocket.close()
-        print("Client Says: Error during connection with server")
-        traceback.print_stack(err.__traceback__)
-        break
+                    self.send_to_server(ERROR,
+                                        "File integrity could not be verified with calculated hash {}".format(calculated_hash))
+                    break
 
-    ClientSocket.close()
+            except Exception as err:
+                self.client_socket.close()
+                print("Client Says: Error during file transport with server")
+                traceback.print_stack(err.__traceback__)
+                break
+
+def main():
+
+    c = ClientProtocol()
+    c.run()
 
 
+if __name__ == "__main__":
+    main()
--- Server/server.py
+++ Server/server.py
@@ -3,144 +3,200 @@
 import hashlib
 import math
 import socket
-import os
+import threading
 import traceback
 from _thread import *
 
 host = '127.0.0.1'
 port = 1233
-ThreadCount = 0
 BufferSize = 1024
 
 File_path = "data/media/"
 Log_path = "data/logs/"
 File_name = 'Test.mp4'
- # open in binary
+# open in binary
 
 SYN = 'Hello'
-AKN = 'Ready'
+AKN_READY = 'Ready'
 AKN_NAME = 'Name'
 AKN_OK = 'Ok'
 AKN_HASH = 'HashOk'
 AKN_COMPLETE = 'SendComplete'
 ERROR = 'Error'
 
-ServerSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-try:
-    ServerSocket.bind((host, port))
-except socket.error as e:
-    print(str(e))
 
-print('Waitiing for a Connection..')
-ServerSocket.listen(5)
+def threadsafe_function(fn):
+    """decorator making sure that the decorated function is thread safe"""
+    lock = threading.Lock()
+
+    def new(*args, **kwargs):
+        lock.acquire()
+        try:
+            r = fn(*args, **kwargs)
+        except Exception as e:
+            raise e
+        finally:
+            lock.release()
+        return r
+
+    return new
 
 
-def threaded_client(connection, idThread):
-    while True:
+class ServerProtocol:
+
+    def __init__(self, clients_number, file_number):
+
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.thread_count = 0
+        self.clients_number = clients_number
+        self.file_number = file_number
+        self.ready_clients = 0
+        self.all_ready_monitor = threading.Event()
 
         try:
-            reply = connection.recv(BufferSize).decode('utf-8')
-            if reply == SYN:
-                print("Server Says: Hail from client {} received".format(idThread))
-                connection.send(str.encode(SYN))
+            self.server_socket.bind((host, port))
+        except socket.error as e:
+            print(str(e))
+
+        print('Waitiing for a Connection..')
+        self.server_socket.listen(5)
+
+    def send_file_to_client(self, connection, thread_id):
+        while True:
+            try:
+                reply = self.receive_from_client(connection)
+                self.verify_reply(reply, SYN)
+
+                self.send_to_client(connection, SYN, "Server Says: Hail from client {} received".format(thread_id))
+
+                reply = self.receive_from_client(connection)
+
+                self.verify_reply(reply, AKN_READY)
+
+                self.update_ready_clients()
+
+                while not self.all_clients_ready(thread_id):
+                    print('Server Says: client {} is put in wait for the rest of clients'.format(thread_id))
+                    self.all_ready_monitor.wait()
+
+                self.send_to_client(connection, File_name, "Server Says: Sending file name ({}) to client "
+                                                           "{}".format(File_name, thread_id))
+
+                reply = self.receive_from_client(connection)
+
+                self.verify_reply(reply, AKN_NAME)
+
+                hash = self.hash_file()
+                self.send_to_client(connection, hash,
+                                    "Server Says: Sending file hash ({}) to client {}".format(hash, thread_id))
+                reply = self.receive_from_client(connection)
+
+                self.verify_reply(reply, AKN_OK)
+
+                size = str(self.get_file_size())
+
+                self.send_to_client(connection, size,
+                                    "Server Says: Sending file size ({}) to client {}".format(size, thread_id))
+                self.send_file(connection, thread_id)
 
                 reply = connection.recv(BufferSize).decode('utf-8')
+                self.verify_reply(reply, AKN_HASH)
+                print("Server Says: File integrity verified by  client {}".format(thread_id))
 
-                if reply == AKN:
+                connection.close()
+                break
 
+            except Exception as err:
+                connection.close()
+                print("Server Says: Error during file transmission to client {}".format(thread_id))
+                traceback.print_stack(err.exc_info)
+                break
 
-                    print("Server Says: Sending file name ({}) to client {}".format(File_name, idThread))
-                    connection.send(str.encode(File_name))
-                    reply = connection.recv(BufferSize).decode('utf-8')
+    def receive_from_client(self, connection):
+        return connection.recv(BufferSize).decode('utf-8')
 
-                    if reply == AKN_NAME:
-                        hash = hash_file()
-                        print("Server Says: Sending file hash ({}) to client {}".format(hash, idThread))
-                        connection.send(str.encode(hash))
+    def send_to_client(self, connection, segment, print_message):
+        connection.send(str.encode(segment))
+        print(print_message)
 
-                        reply = connection.recv(BufferSize).decode('utf-8')
-                        if reply == AKN_OK:
-                            print('sending file')
+    def verify_reply(self, received, expected):
+        if not expected == received:
+            raise Exception("Error in protocol: expected {}; received {}".format(expected, received))
 
-                            size = get_file_size()
-                            connection.send(str.encode(str(size)))
-                            print("Server Says: Sending file size ({}) to client {}".format(size, idThread))
-                            send_file(connection, idThread)
+    @threadsafe_function
+    def all_clients_ready(self, thread_id):
 
-                            reply = connection.recv(BufferSize).decode('utf-8')
-                            if reply == AKN_HASH:
-                                print("Server Says: File successfully sent to client {}".format(hash))
-                                connection.close()
-                                break
+        all_ready = self.clients_number - self.ready_clients == 0
 
+        if all_ready:
+            print("Server Says: all clients ready: starting file transport to client {}".format(thread_id))
 
-            print("Server Says: Unable to connect to client {}".format(idThread))
-            connection.close()
+        return all_ready
 
-        except Exception as err:
-            connection.close()
-            print("Server Says: Error during connection with client {}".format(idThread))
-            traceback.print_stack(err.__traceback__)
-            break
+    def send_file(self, connection, thread_id):
 
+        size = self.get_file_size()
 
-def send_file(connection, idThread):
+        with open(File_path + File_name, 'rb') as file:
+            for _ in range(math.ceil(size / BufferSize)):
+                # read only 1024 bytes at a time
+                chunk = file.read(BufferSize)
+                connection.send(chunk)
 
-    size = get_file_size()
+            print("Server Says: File transmission is complete to client {}".format(thread_id))
+            file.close()
 
-    with open(File_path + File_name, 'rb') as file:
+    def get_file_size(self):
+        with open(File_path + File_name, 'rb') as file:
+            packed_file = file.read()
+        return int(len(packed_file))
+
+    def hash_file(self):
+        file = open(File_path + File_name, 'rb')  # open in binary
+
+        """"This function returns the SHA-1 hash
+        of the file passed into it"""
+        # make a hash object
+        h = hashlib.sha1()
+
+        # open file for reading in binary mode
+
+        # loop till the end of the file
         chunk = 0
-        for _ in range(math.ceil(size / BufferSize)):
+        while chunk != b'':
             # read only 1024 bytes at a time
             chunk = file.read(BufferSize)
-
-            print("Server Says: Sent file chunk {} to  client {}".format(chunk, idThread))
-            connection.send(chunk)
-
-
-        print("Server Says: File transmission is complete")
+            h.update(chunk)
         file.close()
 
+        # return the hex representation of digest
+        return h.hexdigest()
 
-def get_file_size():
-    with open(File_path + File_name, 'rb') as file:
-        packed_file = file.read()
-    return int(len(packed_file))
+    def close(self):
+        self.server_socket.close()
 
-def hash_file():
-    file = open(File_path + File_name, 'rb')  # open in binary
+    @threadsafe_function
+    def update_ready_clients(self):
+        self.ready_clients += 1
 
-    """"This function returns the SHA-1 hash
-    of the file passed into it"""
-    # make a hash object
-    h = hashlib.sha1()
+        if self.clients_number - self.ready_clients == 0:
+            self.all_ready_monitor.set()
 
-    # open file for reading in binary mode
+    def run(self):
 
-    # loop till the end of the file
-    chunk = 0
-    while chunk != b'':
-        # read only 1024 bytes at a time
-        chunk = file.read(BufferSize)
-        h.update(chunk)
-    file.close()
+        while True:
+            print('Listening at', self.server_socket.getsockname())
 
-    # return the hex representation of digest
-    return h.hexdigest()
+            Client, address = self.server_socket.accept()
+            self.thread_count += 1
 
+            print('Connected to: ' + address[0] + ':' + str(address[1]))
 
-def close():
-    ServerSocket.close()
+            if self.thread_count <= self.clients_number:
+                start_new_thread(self.send_file_to_client, (Client, self.thread_count))
+
+            print('Thread Number: ' + str(self.thread_count))
 
 
-while True:
-    print('Listening at', ServerSocket.getsockname())
-
-    Client, address = ServerSocket.accept()
-
-    print('Connected to: ' + address[0] + ':' + str(address[1]))
-
-    start_new_thread(threaded_client, (Client, ThreadCount))
-
-    ThreadCount += 1
-    print('Thread Number: ' + str(ThreadCount))
+s = ServerProtocol(3, 1)
+s.run()
