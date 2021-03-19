a048bdd896dcc03ca203be56590a891cf89ffa91
Juan José Peña D <jj.penad@uniandes.edu.co>
2021-03-19 14:52:34 -0500

    oe

--- Client/client.py
+++ Client/client.py
@@ -11,8 +11,8 @@ from tqdm import tqdm
 from datetime import datetime
 from threading import Thread
 
-host = '127.0.0.1'
-port = 1233
+host = '54.162.149.119'
+port = 60001
 BufferSize = 1024
 
 File_path = "ArchivosRecibidos/"
