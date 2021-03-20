f4076cffea841798237ee95682a494062c3c2aa8
Andres Bayona <al.bayona@uniandes.edu.co>
2021-03-20 13:33:28 -0500

    f

--- Client/client.py
+++ Client/client.py
@@ -28,22 +28,6 @@ ERROR = 'Error'
 AKN_COMPLETE = 'SendComplete'
 
 
-def threadsafe_function(fn):
-    """decorator making sure that the decorated function is thread safe"""
-    lock = threading.Lock()
-
-    def new(*args, **kwargs):
-        lock.acquire()
-        try:
-            r = fn(*args, **kwargs)
-        except Exception as e:
-            raise e
-        finally:
-            lock.release()
-        return r
-
-    return new
-
 
 class ClientProtocol:
 
@@ -220,7 +204,6 @@ class ClientProtocol:
                 self.log_info_c()
                 break
 
-    @threadsafe_function
     def log_info_c(self):
         logging.info(
             '_____________________________________________________________________________________________________')
